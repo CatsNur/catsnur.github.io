932b1aa69c5c647c014f94093a790f5fe7ce1680
Nora Castner <castner.nora@gmail.com>
2025-09-25 15:32:09 +0200

    first template that i like

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Nora Castner | Perception in HCI/AI';
+export const SITE_DESCRIPTION = 'Personal webpage of Dr. Nora Castner, Portfolio Website built with Astro and TailwindCSS. ';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
